d571572e40d5892bdb8f3cd5c8c8b2deabf48d1c
Stefan(W10) <nertzkoban+GitHub@gmail.com>
2019-01-12 12:22:22 -0800

    oh and here's firebase

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from 'firebase/app';
 import * as serviceWorker from './serviceWorker';
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBfti6ehTrhH6h_TKn3Y6IyvEixCL-CB84",
+    authDomain: "jobsearch-winfohackathon.firebaseapp.com",
+    databaseURL: "https://jobsearch-winfohackathon.firebaseio.com",
+    projectId: "jobsearch-winfohackathon",
+    storageBucket: "jobsearch-winfohackathon.appspot.com",
+    messagingSenderId: "518869610756"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
